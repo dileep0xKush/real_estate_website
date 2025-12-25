0e5cbaf8fd833a20153fe442ea70863bc030438e
Dileep-sk <dileep@skylineinfosys.com>
2025-12-25 23:14:55 +0530

    Inital setup for admin panel theme

--- real_estate_nextjs_frontend/src/app/(private)/layout.tsx
+++ real_estate_nextjs_frontend/src/app/(private)/layout.tsx
@@ -3,23 +3,23 @@
 import Header from "@/app/(private)/layouts/Header";
 import Footer from "@/app/(private)/layouts/Footer";
 import type { ReactNode } from "react";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useAuth } from "@/context/AuthProvider";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+// import { useAuth } from "@/context/AuthProvider";
 
 export default function SiteLayout({ children }: { children: ReactNode }) {
-  const { user, loading } = useAuth();
-  const router = useRouter();
+  // const { user, loading } = useAuth();
+  // const router = useRouter();
 
-  useEffect(() => {
-    if (!loading && !user) {
-      router.replace("auth/login");
-    }
-  }, [user, loading, router]);
+  // useEffect(() => {
+  //   if (!loading && !user) {
+  //     router.replace("auth/login");
+  //   }
+  // }, [user, loading, router]);
 
-  if (loading || !user) {
-    return null;
-  }
+  // if (loading || !user) {
+  //   return null;
+  // }
 
   return (
     <>
